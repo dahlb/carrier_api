2e798028cb18e869f3bb0a9b5b66c7f7fe3f322f
Brendan Dahl <dahl.brendan@gmail.com>
2024-12-24 14:25:07 -0500

    feat: add indoor, outdoor unit types to profile

--- src/carrier_api/profile.py
+++ src/carrier_api/profile.py
@@ -14,8 +14,10 @@ class Profile:
     firmware: str = None
     indoor_model: str = None
     indoor_serial: str = None
+    indoor_unit_type: str = None
     outdoor_model: str = None
     outdoor_serial: str = None
+    outdoor_unit_type: str = None
     time_stamp: datetime = None
     zone_ids: [str] = None
     raw_profile_json: dict = None
@@ -37,8 +39,10 @@ class Profile:
         self.firmware = safely_get_json_value(self.raw_profile_json, "firmware")
         self.indoor_model = safely_get_json_value(self.raw_profile_json, "indoorModel")
         self.indoor_serial = safely_get_json_value(self.raw_profile_json, "indoorSerial")
+        self.indoor_unit_type = safely_get_json_value(self.raw_profile_json, "idutype")
         self.outdoor_model = safely_get_json_value(self.raw_profile_json, "outdoorModel")
-        self.outdoor_serial = self.raw_profile_json["outdoorSerial"]
+        self.outdoor_serial = safely_get_json_value(self.raw_profile_json, "outdoorSerial")
+        self.outdoor_unit_type = safely_get_json_value(self.raw_profile_json, "odutype")
         self.time_stamp = isoparse(safely_get_json_value(self.raw_profile_json, "timestamp"))
         self.zone_ids = []
         for zone in safely_get_json_value(self.raw_profile_json, "zones.zone"):
